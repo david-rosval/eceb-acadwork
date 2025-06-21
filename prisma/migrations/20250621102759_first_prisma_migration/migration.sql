1291484274c1e8722e039357c4c2dd5d03386411
david-rosval <20b3010135@untels.edu.pe>
2025-06-21 05:43:24 -0500

    feat: install and configure prisma and the db connection

---
+++ prisma/migrations/20250621102759_first_prisma_migration/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "college" TEXT NOT NULL DEFAULT 'UNTELS',
+    "description" TEXT,
+    "semester" INTEGER NOT NULL,
+    "linkedinUrl" TEXT,
+    "status" TEXT NOT NULL,
+    "rating" DECIMAL(3,2) NOT NULL DEFAULT 0.00,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
