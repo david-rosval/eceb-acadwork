f90bade99e9cfd9c385ef4afa56fed5f32f16c35
david-rosval <20b3010135@untels.edu.pe>
2025-06-21 05:51:05 -0500

    feat: add the seed file and script

---
+++ prisma/seed.ts
@@ -0,0 +1,38 @@
+import { PrismaClient, Prisma } from "../src/generated/prisma";
+
+const prisma = new PrismaClient();
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    email: "johndoe@test.edu.pe",
+    password: "johndoetest",
+    firstName: "John",
+    lastName: "Doe",
+    college: "UNTELS",
+    description: "Test description 1",
+    semester: 5,
+    linkedinUrl: "https://www.linkedin.com/in/johndoe",
+    status: "active",
+    rating: 3.5,
+  },
+  {
+    email: "janedoe@test.edu.pe",
+    password: "janedoetest",
+    firstName: "Jane",
+    lastName: "Doe",
+    college: "UNTELS",
+    description: "Test description 2",
+    semester: 5,
+    linkedinUrl: "https://www.linkedin.com/in/johndoe",
+    status: "active",
+    rating: 4.5,
+  },
+];
+
+export async function main() {
+  for (const u of userData) {
+    await prisma.user.create({ data: u });
+  }
+}
+
+main();
