c81e833e07ce2505ccb220a56cc4ef26b75a3aae
david-rosval <20b3010135@untels.edu.pe>
2025-06-21 02:30:26 -0500

    feat: design login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function LoginPage() {
+  return (
+    <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto w-[350px] space-y-6">
+          <div className="space-y-2 text-center">
+            <h1 className="text-3xl font-bold">Bienvenido a Acadwork</h1>
+            <p className="text-muted-foreground">
+              Ingresa tus credenciales para acceder a tu cuenta
+            </p>
+          </div>
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Correo</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <Label htmlFor="password">Contraseña</Label>
+                <Link
+                  href="/forgot-password"
+                  className="text-sm text-muted-foreground underline hover:text-primary"
+                >
+                  ¿Olvidaste tu contraseña?
+                </Link>
+              </div>
+              <Input id="password" name="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Iniciar Sesión
+            </Button>
+          </form>
+          <div className="text-center text-sm">
+            {"¿No tienes una cuenta? "}
+            <Link href="/register" className="underline hover:text-primary">
+              Crear una cuenta
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/placeholder.svg?height=1080&width=1920"
+          alt="Login illustration"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
