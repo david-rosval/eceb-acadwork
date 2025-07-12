99ffea7e8dd19743578e630a8404cb50591471f5
david-rosval <20b3010135@untels.edu.pe>
2025-07-12 02:10:10 -0500

    chore: change seed file to create the categories

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,39 +2,20 @@ import { PrismaClient, Prisma } from "../src/generated/prisma";
 
 const prisma = new PrismaClient();
 
-const userData: Prisma.UserCreateInput[] = [
-  {
-    email: "johndoe@test.edu.pe",
-    password: "johndoetest",
-    firstName: "John",
-    lastName: "Doe",
-    college: "UNTELS",
-    major: "Ing. de Sistemas", 
-    description: "Test description 1",
-    semester: 5,
-    linkedinUrl: "https://www.linkedin.com/in/johndoe",
-    status: "active",
-    rating: 3.5,
-  },
-  {
-    email: "janedoe@test.edu.pe",
-    password: "janedoetest",
-    firstName: "Jane",
-    lastName: "Doe",
-    college: "UNTELS",
-    major: "Ing. Ambiental",
-    description: "Test description 2",
-    semester: 5,
-    linkedinUrl: "https://www.linkedin.com/in/johndoe",
-    status: "active",
-    rating: 4.5,
-  },
+const categoryData: Prisma.CategoryCreateInput[] = [
+  { name: "Programación" },
+  { name: "Diseño Gráfico" },
+  { name: "Cálculo" },
+  { name: "Física"  },
+  { name: "Química"  },
+  { name: "Estadística"  },
+  { name: "IoT"  },
+  { name: "Dibujo técnico" },
 ];
 
-
 export async function main() {
-  for (const u of userData) {
-    await prisma.user.create({ data: u });
+  for (const u of categoryData) {
+    await prisma.category.create({ data: u });
   }
 }
 
